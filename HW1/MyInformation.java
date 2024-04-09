efb08b733706a5b572c816e858c50d30f715dbc6
Hoang Son <hs.hoangson15062005@gmail.com>
2024-04-09 17:57:56 +0700

    homework done

---
+++ HW1/MyInformation.java
@@ -0,0 +1,15 @@
+package HW1;
+
+public class MyInformation {
+    public static void main(String[] args) {
+        System.out.print("Họ và tên: Hoàng Sơn\t");
+        System.out.print("Mã SV: 23020423/t");
+        System.out.print("Lớp: K68AI1\t");
+        System.out.print("Username: Hoangson1506\t");
+        for(int i=9; i>=1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
